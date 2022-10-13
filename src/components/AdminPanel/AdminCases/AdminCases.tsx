a51fbfb6e60d7fa656b45c5d30d98eaf9e1f98ea
Vera Krasnova <azirafail@mail.ru>
2022-10-13 22:58:42 +0300

    feature/CSR-15: add modal to adminCases component

--- src/components/AdminPanel/AdminCases/AdminCases.tsx
+++ src/components/AdminPanel/AdminCases/AdminCases.tsx
@@ -1,8 +1,30 @@
 import { Box, Divider } from "@mui/material";
+import { useState } from "react";
 import { casesForAdmin } from "../../../mocks/casesForAdmin";
-import { CaseCreateButton, CasesSearchForm, Case } from "./components";
+import { CaseCreateButton, CasesSearchForm, Case, ModalAdmin } from "./components";
 
 export const AdminCases = () => {
+
+  const [cases, setCases] = useState(casesForAdmin);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  // если кейс есть, то открываю модалку для редактирования этого кейса
+  // и передаю его в контент модалки, если null то открываю модалку для создания нового кейса
+  const [editableCase, setEditableCase] = useState<any>(null); 
+
+  const handleCreateButton = () => {
+    console.log('create button clicked');
+    setEditableCase(null);
+    setModalOpen(true);
+  }
+
+  const handleCaseClick = (caseId: number) => {
+    console.log('case clicked');
+    const currentCase = cases.find(item => item.id === caseId)
+    setEditableCase(currentCase);
+    setModalOpen(true);
+  }
+
   return(
     <Box className='admin-cases'>
       <Box component='h1' className='admin-cases__title'>Кейсы</Box>
@@ -10,14 +32,21 @@ export const AdminCases = () => {
       <CasesSearchForm></CasesSearchForm>
 
       <Box className="admin-cases__cases-container">
-        <CaseCreateButton onClick={() => console.log('create button clicked')}/>
+        <CaseCreateButton onClick={handleCreateButton}/>
 
-        {casesForAdmin.map((caseItem) => {
-          return <Case key={caseItem.id} caseItem={caseItem} onClick={() => console.log('case clicked')}></Case>
+        {cases.map((caseItem) => {
+          return <Case key={caseItem.id} caseItem={caseItem} onClick={() => handleCaseClick(caseItem.id)}></Case>
         })}
 
       </Box>
+      <ModalAdmin 
+        open={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        title={editableCase ? `Кейс "${editableCase?.title}"` : 'Кейс'}>
+        МОДАЛКА
+      </ModalAdmin>
       
     </Box>
   )
 }
+
