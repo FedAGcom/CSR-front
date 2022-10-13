58c6f1d5224c29d0a17ea50b1a20b40e18abe024
Vera Krasnova <azirafail@mail.ru>
2022-10-13 23:54:21 +0300

    feature-CSR-15: add custom input and select for admin

---
+++ src/components/AdminPanel/AdminCases/components/AdminSelect.tsx
@@ -0,0 +1,41 @@
+import { SxProps, Select as MUISelect, SelectProps} from '@mui/material';
+import { forwardRef } from 'react';
+
+export const AdminSelect = forwardRef<JSX.Element, SelectProps>(function Select(props: SelectProps, ref) {
+  const selectStyles: SxProps = {
+    boxSizing: 'border-box',
+    borderRadius: '5px',
+    width: '100%',
+    color: '#8A8A8A',
+    background: 'transparent',
+    border: '1px solid #fff',
+    fontFamily: 'inherit',
+    fontSize: '16px',
+    lineHeight: '20px',
+    padding: '0 !important',
+    '& div': {
+      padding: '9px 15px',
+    },
+    '& svg path': {
+      fill: '#fff',
+    },
+    '&.Mui-focused fieldset': {
+      border: 'none',
+      outline: 'none !important',
+    },
+    '&.Mui-focused': {
+      color: '#fff',
+    },
+    '&.Mui-error': {
+      border: '1px solid #FB0000',
+    },
+  };
+
+  return (
+    <MUISelect 
+      {...props} 
+      sx={{ ...selectStyles, ...props.sx }} 
+      ref={ref}
+    />
+  );
+});
