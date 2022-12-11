954846d7d3b6985b53328e5df0bec977ad870d6d
Vladislav Kulyakin <kulyakinv@gmail.com>
2022-12-11 14:00:57 +0300

    feature/CSRFR-42: fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,7 +9,7 @@ const $api = axios.create({
 });
 
 $api.interceptors.request.use((config: AxiosRequestConfig) => {
-  config.headers!.Authorization = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3NjU2MTE5ODA1MzQ4MTE4NiIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjcwNDUyMTc2LCJleHAiOjE2NzEwNTY5NzZ9.r-GDUDHavxySg5EySqovh3pWJg7WtVGuqxAKxHGPrK0';
+  config.headers!.Authorization = Cookies.get('AuthorizationCSRApp');
   return config;
 });
 
