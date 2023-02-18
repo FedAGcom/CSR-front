b87c2adae7eb8bf2f134f2ca90453c1afd26aae4
Vanilio1997 <Vlad.rabota.96@yandex.ru>
2023-02-18 16:46:58 +0300

    Added eng banner

--- src/components/images/index.ts
+++ src/components/images/index.ts
@@ -3,6 +3,7 @@ import flagRu from './flag-ru.png';
 import flagEn from './flag-en.png';
 import weapon from './weapon.png';
 import backgroundMain from './backgroundMain.png';
-import banner from './banner.png';
+import bannerRu from './banner-ru.png';
+import bannerEn from './banner-en.png';
 
-export { caseImage, flagRu, flagEn, weapon, backgroundMain, banner };
+export { caseImage, flagRu, flagEn, weapon, backgroundMain, bannerRu, bannerEn };
